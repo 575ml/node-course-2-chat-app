5c4e0775e347e3564517fe53800f27eb75ad3003
575ml <aiza.espineda@gmail.com>
2018-04-16 10:07:30 +1000

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,12 +1,29 @@
 const express    = require('express'),
+	  http		 = require('http'),
+	  socketIO	 = require('socket.io'),
 	  app 	  	 = express(),
 	  path 	  	 = require('path'),
 	  publicPath = path.join(__dirname, '../public');
 
 const port = process.env.PORT || 5000;
 
+var server = http.createServer(app),
+	io 	   = socketIO(server);
+
+//APP
 app.use(express.static(publicPath));
+
 // app.set("view engine", "hbs");
+
+
+io.on('connection', (socket) => {
+	console.log('New user connected')
+
+	socket.on('disconnect', (socket) => {
+	console.log("User was disconnected")
+	});
+});
+
 
 //ROUTES
 
@@ -16,6 +33,6 @@ app.get('/about', (req, res) => {
 });
 
 //SERVER
-app.listen(port, () => {
+server.listen(port, () => {
 	console.log(`CHAT running on poty: ${port}`);
 });
