28f402435bb5941dec0c12c3c0d380054c2c4192
575ml <aiza.espineda@gmail.com>
2018-04-16 14:27:58 +1000

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -13,5 +13,28 @@ socket.on('disconnect', function(){
 //New message
 socket.on('newMessage', function(message){
 	console.log('New message', message);
+	var li = jQuery('<li></li>');
+	li.text(`${message.from}: ${message.text}`);
+
+	jQuery('#messages').append(li);
 });
 
+//Acknowledgements
+// socket.emit('createMessage', {
+// 	from: 'Frank',
+// 	text: 'Hi'
+// }, function(data) {
+// 	console.log('got it', data)
+// })
+
+
+jQuery('#message-form').on('submit', function(e) {
+	e.preventDefault();
+
+	socket.emit('createMessage', {
+		from: 'User',
+		text: jQuery('[name=message]').val()
+	}, function () {
+
+	});
+});
