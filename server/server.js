31eaf01ecf306f6d311fcf7a93c5cceb08bf6643
575ml <aiza.espineda@gmail.com>
2018-04-16 11:25:18 +1000

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -25,14 +25,31 @@ io.on('connection', (socket) => {
 	console.log("User was disconnected");
 	});
 
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'Welcome to the chat!',
+		createAt: new Date().getTime()
+	});
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New user joined',
+		createdAt: new Date().getTime()
+	})
+
 	//create message
 	socket.on('createMessage', (message) => {
 		console.log('createMessage', message);
-		io.emit('newMessage', {
-			from: message.from,
-			text: message.text,
-			createdAt: new Date().getTime()
-		});
+		// io.emit('newMessage', {
+		// 	from: message.from,
+		// 	text: message.text,
+		// 	createdAt: new Date().getTime()
+		// });
+		// socket.broadcast.emit('newMessage', {
+		// 	from: message.from,
+		// 	text: message.text,
+		// 	createdAt: new Date().getTime()
+		// });
 	});
 });
 
